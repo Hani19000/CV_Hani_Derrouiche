2306ee0751dbb0a8c34622c24e7996980799bcf0
Hani <hanider27@gmail.com>
2025-10-30 00:49:35 +0100

    mise a jours

--- CV_Projects/settings.py
+++ CV_Projects/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%i4v!7%n442f=hhl9_-)3@h-6km(=607^6(wxm+&&58yh%1nx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
